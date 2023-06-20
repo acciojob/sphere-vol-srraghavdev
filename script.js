94628b7cc7e1538c4baeff23b1d9d6e73fb55111
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-06-20 14:17:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function volume_sphere() {
     let radref = document.getElementById('radius')
     let rad= radref.value
     let volref= document.getElementById("volume")
-    volref.value=((4/3)*(22/7)*(rad*rad*rad))
+    volref.value=((4/3)*(3.1415926535898)*(rad*rad*rad)).toFixed(4)
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
